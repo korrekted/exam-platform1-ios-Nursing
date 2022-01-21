dfdf0502a16264237b4c7b0714ac335f75e0ce96
akonst17 <akonst17@gmail.com>
2022-01-21 13:58:47 +0300

    migrate RushSDK -> OtterScale

---
+++ Nursing/Classes/Service/Profile/Request/LoginRequest.swift
@@ -0,0 +1,8 @@
+//
+//  LoginRequest.swift
+//  Nursing
+//
+//  Created by Андрей Чернышев on 21.01.2022.
+//
+
+import Foundation
---
+++ Nursing/Classes/Service/Profile/Request/SyncTokensRequest.swift
@@ -0,0 +1,8 @@
+//
+//  SyncTokensRequest.swift
+//  Nursing
+//
+//  Created by Андрей Чернышев on 21.01.2022.
+//
+
+import Foundation
