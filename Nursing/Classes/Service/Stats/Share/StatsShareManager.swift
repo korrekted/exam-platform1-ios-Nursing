d41e5fcc291f7883078a4404afcdf8eba9112592
Sergei Pankov <sergeipankov@Sergeis-MacBook-Air.local>
2022-04-11 16:38:00 +0100

    новый акк; build 44

--- Nursing/Classes/Service/Stats/Share/StatsShareManager.swift
+++ Nursing/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.nclex.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.nursing.widget")
     }
     
     @discardableResult
