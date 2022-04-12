beab8e47341f0efe4f594773f55904afe1676a2e
akonst17 <akonst17@gmail.com>
2022-04-12 12:47:42 +0300

    платный доступ учитывает paypal; build 45

--- Nursing/Classes/Service/Session/Manager/SessionManagerCore.swift
+++ Nursing/Classes/Service/Session/Manager/SessionManagerCore.swift
@@ -43,9 +43,11 @@ extension SessionManagerCore {
         let subscriptions = paymentData.subscriptions.appleAppStore
             + paymentData.subscriptions.googlePlay
             + paymentData.subscriptions.stripe
+            + paymentData.subscriptions.paypal
         let nonConsumables = paymentData.nonConsumables.appleAppStore
             + paymentData.nonConsumables.googlePlay
             + paymentData.nonConsumables.stripe
+            + paymentData.nonConsumables.paypal
 
         let hasValidSubscription = subscriptions.contains(where: { $0.valid })
         let hasValidNonConsumable = nonConsumables.contains(where: { $0.valid })
